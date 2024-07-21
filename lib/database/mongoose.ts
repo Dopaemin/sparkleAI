2893120327c0bb4e57e056377f149643343acbe6
Dopaemin <oklopcicemin@gmail.com>
2024-07-21 20:17:30 +0200

    change

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -21,7 +21,7 @@ export const connectToDatabase = async () => {
     if(!MONGODB_URL) throw new Error('Missing MONGODB_URL');
 
     cached.promise = cached.promise || mongoose.connect(
-        MONGODB_URL, { dbName: 'sparkleai', bufferCommands: false})
+        MONGODB_URL, { dbName: 'Sparkleai', bufferCommands: false})
     
     cached.conn = await cached.promise;
 
